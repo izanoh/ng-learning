911194d301ca42802c2dba679a0f4ea6c7dbbcba
Sathaporn W <wongsanoh.nut@gmail.com>
2018-07-04 22:01:14 +0700

    add regis form

--- src/app/validate/validate.component.ts
+++ src/app/validate/validate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-validate',
   templateUrl: './validate.component.html',
@@ -6,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidateComponent implements OnInit {
 
-  constructor() { }
+  email: String;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      selector: '',
+      name: '',
+      email: '',
+      message: 'default',
+
+    });
+
+    this.form.valueChanges.subscribe(console.log);
   }
 
 }
