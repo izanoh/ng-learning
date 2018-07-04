1a72e2b2832073f882aafa4cf9d9d96fdcf857b6
Sathaporn W <wongsanoh.nut@gmail.com>
2018-07-04 21:50:01 +0700

    [Add] validate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ValidateComponent } from './validate/validate.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { ValidateComponent } from './validate/validate.component';
     ValidateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
